598e1dbfbace48c8dde8baf6bbb8d765697f1d67
Jason 'Toolbox' Oettinger <jason@oettinger.email>
2016-09-14 13:34:03 -0400

    Include OrderbookSync. Now it's time to begin full integration

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { default as PublicClient        } from './lib/clients/public';
 export { default as WebsocketClient     } from './lib/clients/websocket';
 export { default as AuthenticatedClient } from './lib/clients/authenticated';
 export { default as Orderbook           } from './lib/orderbook';
-//export { default as OrderbookSync       } from './lib/orderbook_sync';
+export { default as OrderbookSync       } from './lib/orderbook_sync';
